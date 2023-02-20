88fdec037bc49c0870b32ce94d139103b3badb0f
Mateusz Serafin <mserafin@zstarnob.onmicrosoft.com>
2023-02-20 14:11:29 +0100

    testy jednostkowe dodana struktura podstawowa

---
+++ Testy Jednostkowe/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Testy_Jednostkowe
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Testy/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Testy
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
