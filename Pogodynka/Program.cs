b015cb9ab9a7ac1a3ea749e725edcaad442c46e7
Mateusz Serafin <mserafin@zstarnob.onmicrosoft.com>
2023-02-20 14:07:37 +0100

    lekcja test dzialania gita

--- Pogodynka/Program.cs
+++ Pogodynka/Program.cs
@@ -2,4 +2,5 @@
 WeatherAPI api = new WeatherAPI("Rzeszów");
 api.connect();
 Console.Clear();
+Console.Clear();
 Console.WriteLine("["+api.City()+"] " + "(" + api.getCurrentWeatherCondition() + ") " + "Temperatura Odczuwalna: " + api.getCurrentTemperature() + "  Ciśnienie: " + api.getCurrentPressure() + " Wiatr: " + api.getCurrentWind() + " Wilgotność: " + api.getCurrentAirHumidity());
